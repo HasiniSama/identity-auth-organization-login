8f0f0ced699c3587bb08376865579ede0879bcf8
Hasini Samarathunga <hasinis@wso2.com>
2025-05-29 17:30:51 +0530

    Add review suggestions

--- components/org.wso2.carbon.identity.application.authenticator.organization.login/src/main/java/org/wso2/carbon/identity/application/authenticator/organization/login/constant/AuthenticatorConstants.java
+++ components/org.wso2.carbon.identity.application.authenticator.organization.login/src/main/java/org/wso2/carbon/identity/application/authenticator/organization/login/constant/AuthenticatorConstants.java
@@ -58,9 +58,12 @@ public class AuthenticatorConstants {
     public static final String ENABLE_CONFIG = ".enable";
     public static final String LOGIN_HINT_PARAMETER = "login_hint";
     public static final String SP_ID_PARAMETER = "spId";
+    public static final String DEFAULT_PARAM = "defaultParam";
 
     public static final String ORGANIZATION_LOGIN_FAILURE = "organizationLoginFailure";
     public static final String ERROR_MESSAGE = "&authFailure=true&authFailureMsg=";
+    public static final String INVALID_ORGANIZATION_NAME_ERROR_MESSAGE = "Invalid Organization Name";
+    public static final String INVALID_ORGANIZATION_HANDLE_ERROR_MESSAGE = "Invalid Organization Handle";
 
     public static final String REQUEST_ORG_PAGE_URL = "authenticationendpoint/org_name.do";
     public static final String REQUEST_ORG_HANDLE_PAGE_URL = "authenticationendpoint/org_handle.do";
@@ -76,4 +79,13 @@ public class AuthenticatorConstants {
     public static final String OIDC_CLAIM_DIALECT_URL = "http://wso2.org/oidc/claim";
 
     public static final String SAML_RESP = "SAMLResponse";
+
+    /**
+     * All discovery routing types.
+     */
+    public enum RoutingType {
+        ORG_NAME,
+        ORG_HANDLE,
+        ORG_DOMAIN
+    }
 }
